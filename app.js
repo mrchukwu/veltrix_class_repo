69f4e7ce9203db3d645742668f4a6381a36fdf54
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-11-11 20:44:13 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Type conversion
+
+
+//Implicity Conversion
+
+const a = "5";
+const b = 10;
+const c = 20;
+const d = 10;
+
+
+
+const result = b + c + Number(a); // "510" (string) BODMAS BIDMAS
+// console.log(result);
+
+const addNumber = function (num1, num2){
+    console.log(Number(num1) + Number(num2));
+}
+
+
+const func = function (num1, num2){
+
+    const add = addNumber(num1, num2);
+
+    console.log(add);
+}
+
+func(5, 10);
+
+// 01
