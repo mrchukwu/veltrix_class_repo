d739bcd89b18a2f957dbe2b1c6eb6c7cb7533d31
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-11-12 19:50:33 +0100

    update on string.js file

---
+++ string.js
@@ -0,0 +1,33 @@
+const firsname = 'Kennedy'; //single quotes
+const lastname = "Chukwu"; //double quotes
+
+const fullname =  "My firstname is\n\n\n" 
++ firsname + " " + lastname;
+
+// console.log(fullname);
+
+// console.log('------------------');
+ 
+const fullname2 = `My fullname is ${firsname} ${lastname}`
+
+console.log(fullname2);
+console.log(fullname2.includes('Kennedy'));
+console.log(fullname2.toLowerCase());
+console.log(fullname2.toUpperCase());
+console.log(fullname2.endsWith('chukwu'.toUpperCase()));
+
+
+
+
+
+
+
+
+
+// const checkWhatIsInThisPosition = fullname.charAt(4)
+//  console.log(checkWhatIsInThisPosition);
+
+// console.log(fullname[7]);
+
+// Number()
+
